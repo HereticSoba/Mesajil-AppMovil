f7445b8145dd8d90dfd2dde9cbdbb8ec2f7f8cbb
Diego Farid Solorzano Villegas <PT72787033@idat.pe>
2026-08-09 14:01:27 -0500

    Modificación del API para el despliegue.

--- Backend/MesajilApi/Program.cs
+++ Backend/MesajilApi/Program.cs
@@ -8,7 +8,10 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
+if(builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+}
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -92,11 +95,8 @@ builder.Services.AddHttpClient<IPagoService, PagoService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 //Deshabilitado porque me arroja error ya que el programa quiere el certificado HTTPS (por ahora no hay)
 //app.UseHttpsRedirection();
